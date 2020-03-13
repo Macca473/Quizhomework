65efd971a18fa6c8753fe85938a4b11c81732a45
Matthew <mmacca473@gmail.com>
2020-03-13 14:56:36 +1030

    made the timer and put down Question answer placeholders

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
 
+  //cheeky consolelog
+
   console.log("T");
 
-  // var countdown = function(){
-  //     var sttime = 30;
-
-  //     var cdtime = sttime;
-  //     // for(cdtime = sttime; cdtime--; cdtime > 0) {
-  //     setInterval(minusint,1000);
-  //     // }
-      
-  //     return cdtime;
-  // }
-
-  // function minusint() {
-  //   countdown(cdtime) = countdown(cdtime);
-  //   countdown(cdtime)--;
-  // }
-    
-
-
-  // console.log(countdown());
-
-
-  // $("#countdowntime").append(countdown());
+  //timer 
 
   function countdown(tm) {
-    var si = setInterval(function () {
-      tm = $("#countdowntime").text();
-      console.log(tm);
-      if(tm < 0) {
+    var tm = 30;
+    var si = setInterval(function setint() {
+      tm--;
+      $("#countdowntime").text(tm);
+      console.log("inside tm " + tm);
+      if(tm <= 0) {
         clearInterval(si);
       }
     },1000)
-    return tm
   }
 
   $("#countdowntime").append(countdown());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
