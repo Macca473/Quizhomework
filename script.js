202c4b881c0182c377c7cc71fa90d03424705bce
Matthew <mmacca473@gmail.com>
2020-03-15 13:54:27 +1030

    keep forgeting to push

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     var si = setInterval(function setint() {
       tm--;
       $("#countdowntime").text(tm);
-      console.log("inside tm " + tm);
+      // console.log("inside tm " + tm);
       if(tm <= 0) {
         clearInterval(si);
       }
@@ -20,7 +20,85 @@ $(document).ready(function() {
 
   $("#countdowntime").append(countdown());
 
+  //////////////////////////////Question
 
+  function questionarray() {
+    var question = ["how many legs does a cat have?"
+                  , "how many cats does a leg have?"
+                  ];
+    $("#ques").text(question[0]);
+    console.log(question[0]);
+  }
+
+  $("#ques").append(questionarray());
+
+  //////////////////////////////Answer 0
+
+  $("#answ0").click(function() { 
+    console.log("clicked");
+    $("#answ0").toggleClass("cli");
+  });
+
+  function answer0array() {
+    var answer0 = ["1"
+                  ,"3"
+                  ];
+    $("#answ0").text(answer0[0]);
+    console.log(answer0[0]);
+  }
+
+  $("#answ0").append(answer0array());
+
+  //////////////////////////////Answer 1
+
+  $("#answ1").click(function() { 
+    console.log("clicked");
+    $("#answ1").toggleClass("cli");
+  });
+
+  function answer1array() {
+    var answer1 = ["4"
+                  , "12"
+                  ];
+    $("#answ1").text(answer1[0]);
+    console.log(answer1[0]);
+  }
+
+  $("#answ1").append(answer1array());
+
+  //////////////////////////////Answer 2
+
+  $("#answ2").click(function() { 
+    console.log("clicked");
+    $("#answ2").toggleClass("cli");
+  });
+
+  function answer2array(num) {
+    var answer2 = ["9"
+                  , "43"
+                  ];
+    $("#answ2").text(answer2[0]);
+    console.log(answer2[0]);
+  }
+
+  $("#answ2").append(answer2array());
+
+  //////////////////////////////Answer 3
+
+  $("#answ3").click(function() { 
+    console.log("clicked");
+    $("#answ3").toggleClass("cli");
+  });
+
+  function answer3array(num) {
+    var answer3 = ["5"
+                  , "what? that makes no sense"
+                  ];
+    $("#answ3").text(answer3[0]);
+    console.log(answer3[0]);
+  }
+
+  $("#answ3").append(answer3array());
 
 
 
