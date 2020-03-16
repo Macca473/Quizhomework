40d29eab63d8b8e6cf096a3dca5fe0d03b6cd23c
Matthew <mmacca473@gmail.com>
2020-03-16 13:46:26 +1030

    set up the correct/incorrect conditions

--- script.js
+++ script.js
@@ -12,28 +12,22 @@ $(document).ready(function() {
       tm--;
       $("#countdowntime").text(tm);
       // console.log("inside tm " + tm);
-      if(tm <= 0) {
+      if(tm <= 0, ) {
         clearInterval(si);
+        restart();
       }
     },1000)
   }
 
   $("#countdowntime").append(countdown());
 
-  //////////////////////////////Correct Answer
+  //////////////////////////////Answer Check
 
-  function answercheck() {
-    var tf = false;
-    var correct = ["4"
-                  ,"what? that makes no sense"
-                  ];
+  var selec = "";
 
-    if ($("cli").text() = 4) {
-      tf = true };
-      return tf
-  }
+  //////////////////////////////Question Number
 
-  
+  var Quesnum = "0";
 
   //////////////////////////////Question
 
@@ -41,8 +35,8 @@ $(document).ready(function() {
     var question = ["how many legs does a cat have?"
                    , "how many cats does a leg have?"
                    ];
-    $("#ques").text(question[0]);
-    console.log(question[0]);
+    $("#ques").text(question[Quesnum]);
+    console.log(question[Quesnum]);
     
   }
 
@@ -53,15 +47,17 @@ $(document).ready(function() {
   $("#answ0").click(function() { 
     console.log("clicked Answer 0");
     $("#answ0").toggleClass("cli");
-    console.log("answer " + answercheck());
+    selec = $("#answ0").text();
+    answercheck();
+    console.log("answer " + selec + " " + answercheck());
   });
 
   function answer0array() {
     var answer0 = ["1"
                   ,"3"
                   ];
-    $("#answ0").text(answer0[0]);
-    console.log(answer0[0]);
+    $("#answ0").text(answer0[Quesnum]);
+    console.log(answer0[Quesnum]);
   }
 
   $("#answ0").append(answer0array());
@@ -71,15 +67,17 @@ $(document).ready(function() {
   $("#answ1").click(function() { 
     console.log("clicked Answer 1");
     $("#answ1").toggleClass("cli");
-    console.log("answer " + answercheck());
+    selec = $("#answ1").text();
+    answercheck();
+    console.log("answer " + selec + " " + answercheck());
   });
 
   function answer1array() {
     var answer1 = ["4"
                   , "12"
                   ];
-    $("#answ1").text(answer1[0]);
-    console.log(answer1[0]);
+    $("#answ1").text(answer1[Quesnum]);
+    console.log(answer1[Quesnum]);
   }
 
   $("#answ1").append(answer1array());
@@ -89,15 +87,17 @@ $(document).ready(function() {
   $("#answ2").click(function() { 
     console.log("clicked Answer 2");
     $("#answ2").toggleClass("cli");
-    console.log("answer " + answercheck());
+    selec = $("#answ2").text();
+    answercheck();
+    console.log("answer " + selec + " " + answercheck());
   });
 
   function answer2array(num) {
     var answer2 = ["9"
                   , "43"
                   ];
-    $("#answ2").text(answer2[0]);
-    console.log(answer2[0]);
+    $("#answ2").text(answer2[Quesnum]);
+    console.log(answer2[Quesnum]);
   }
 
   $("#answ2").append(answer2array());
@@ -107,20 +107,43 @@ $(document).ready(function() {
   $("#answ3").click(function() { 
     console.log("clicked Answer 3");
     $("#answ3").toggleClass("cli");
-    console.log("answer " + answercheck());
+    selec = $("#answ3").text();
+    answercheck();
+    console.log("answer " + selec + " " + answercheck());
   });
 
   function answer3array(num) {
     var answer3 = ["5"
                   , "what? that makes no sense"
                   ];
-    $("#answ3").text(answer3[0]);
-    console.log(answer3[0]);
+    $("#answ3").text(answer3[Quesnum]);
+    console.log(answer3[Quesnum]);
 
   }
 
   $("#answ3").append(answer3array());
 
+  //////////////////////////////Answer Check
+
+  function answercheck() {
+    var tf = false;
+    var correct = ["4"
+                  ,"what? that makes no sense"
+                  ];
+    if (selec == correct[Quesnum]) {
+      tf = true;
+      Quesnum = Quesnum + 1;
+    } else {
+      restart();
+    }
+      return tf
+  }
+
+  ////////////////////////////// Fail
+
+  function restart() {
+    Quesnum = "0";
+  }
 
 
 
